57f5a8f18961a42252c429d1235c594930f923d7
hetvi-gandhi <155214071+hetvi-gandhi@users.noreply.github.com>
2024-01-02 10:46:45 +0530

    Add files via upload

---
+++ demo.php
@@ -0,0 +1,15 @@
+<?php
+
+ $name=$_REQUEST['my_name'];
+ "<br>";
+ $city=$_REQUEST['my_city'];
+ "<br>";
+ $address=$_REQUEST['my_add'];
+
+echo "<table border=1 bordercolor='blue'> <tr><td>name</td><td>$name</td></tr>
+		<tr><td>city</td><td>$city</td></tr>
+		<tr><td>address</td><td>$address</td></tr>
+		</table>"
+
+
+?>
---
+++ demo_1.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<form action="demo.php" method="get">
+	enter name<input type="text" name="my_name"><br><br>
+	enter city<input type="text" name="my_city"><br><br>
+	enter address<input type="type" name="my_add"><br><br>
+	
+	<input type="submit" name="submit">
+	
+	
+	</form>
+</body>
+</html>
